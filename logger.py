e8c4372ec6f916324ef171f3e43be4e6f0b3ce0d
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-04 15:33:53 -0300

    Actualizar logger.py

--- logger.py
+++ logger.py
@@ -4,12 +4,14 @@ class Logger:
     def __init__(self):
         self.log_file = "database/system.log"
 
-    def _write(self, level, msg):
-        now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        entry = f"[{now}] [{level}] {msg}\n"
-        print(entry, end="")
+    def _log(self, level, msg):
+        time_str = datetime.datetime.now().strftime("%H:%M:%S")
+        entry = f"[{time_str}] [{level}] {msg}"
+        print(entry)
         with open(self.log_file, "a") as f:
-            f.write(entry)
+            f.write(entry + "\n")
 
-    def info(self, msg): self._write("INFO", msg)
-    def error(self, msg): self._write("ERROR", msg)
+    def info(self, msg): self._log("INFO", msg)
+    def error(self, msg): self._log("ERROR", msg)
+
+log = Logger()
