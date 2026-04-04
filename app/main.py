8e014b5d3eeb8e28b85932b2a78f43ed6e51dde0
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-04 18:27:01 -0300

    Actualizar main.py

--- app/main.py
+++ app/main.py
@@ -1,14 +1,18 @@
 import os
 from flask import Flask
-from app.routes import bp
 
 def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
-    app.secret_key = "mally_secret_v18"
     
-    # Asegurar carpetas de media
+    # CONFIGURACIÓN DE PODER: Permite subir videos de hasta 100GB (Ajustable)
+    app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024 * 1024 
+    
+    # Registrar las rutas simplificadas
+    from app.routes import bp
+    app.register_blueprint(bp)
+    
+    # Asegurar que las carpetas existan al arrancar
     os.makedirs('media/raw', exist_ok=True)
     os.makedirs('media/exports', exist_ok=True)
     
-    app.register_blueprint(bp)
     return app
