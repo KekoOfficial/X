3175fcb0330fa317b562a1f2b9ad227f45939e39
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-04 15:33:35 -0300

    Actualizar data.py

--- data.py
+++ data.py
@@ -1,23 +1,25 @@
 import json
 import os
-
-DB_PATH = "database/config.json"
+from config import DB_PATH
 
 class NexusDB:
     def __init__(self):
-        os.makedirs("database", exist_ok=True)
+        self._cached_data = None
         if not os.path.exists(DB_PATH):
             self.save({"bot_token": "", "chat_id": ""})
 
     def get(self):
+        if self._cached_data: return self._cached_data
         try:
             with open(DB_PATH, "r") as f:
-                return json.load(f)
+                self._cached_data = json.load(f)
+                return self._cached_data
         except:
             return {"bot_token": "", "chat_id": ""}
 
     def save(self, data):
         with open(DB_PATH, "w") as f:
             json.dump(data, f, indent=4)
+        self._cached_data = data
 
 db = NexusDB()
