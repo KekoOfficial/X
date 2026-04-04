85eeb66b645efb00204e410f7b82eb15de2d5bb4
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-04-04 19:01:44 -0300

    Actualizar routes.py

--- app/routes.py
+++ app/routes.py
@@ -12,22 +12,19 @@ def index():
 
 @bp.route('/api/brutal-process', methods=['POST'])
 def brutal_process():
-    # Buscamos 'video_file' que es el nombre que pusimos en el HTML
     if 'video_file' not in request.files:
-        return "Error: No se encontró el archivo en la solicitud (400)", 400
+        return "Error 400: No hay video", 400
     
     file = request.files['video_file']
     if file.filename == '':
         return redirect(url_for('main.index'))
 
-    # 1. Guardado ultra-rápido
     filename = secure_filename(file.filename)
     raw_path = os.path.join('media', 'raw', filename)
     os.makedirs(os.path.dirname(raw_path), exist_ok=True)
-    file.save(raw_path)
+    file.save(raw_path) #
     
-    # 2. DISPARO ASÍNCRONO (Cero espera para el navegador)
-    # El motor trabaja mientras tú ya estás de vuelta en la pantalla principal
+    # DISPARO ASÍNCRONO SIMPLE
     thread = threading.Thread(target=VideoEngine.execute_parallel_cut, args=(raw_path, 60))
     thread.daemon = True
     thread.start()
